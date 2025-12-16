089941a667ad8a15188658e6ab649cb6686c9a24
jungsa <106296196+knkevol@users.noreply.github.com>
2025-12-16 14:58:37 +0900

    위젯 Network 준비

--- Source/Robo_CopyProject/Robo_CopyProject.Build.cs
+++ Source/Robo_CopyProject/Robo_CopyProject.Build.cs
@@ -8,7 +8,7 @@ public class Robo_CopyProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AnimGraphRuntime", "NavigationSystem", "AIModule", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
