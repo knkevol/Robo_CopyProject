0a568dc01122acb5a1d71ca772544675ae99e9e3
jungsa <106296196+knkevol@users.noreply.github.com>
2026-01-14 19:33:54 +0900

    ABP 세팅

---
+++ Source/Robo_CopyProject/Player/RoboAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "RoboAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBO_COPYPROJECT_API URoboAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
