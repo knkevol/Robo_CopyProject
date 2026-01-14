83146b030094cba7809591e068adf96d603d1943
jungsa <106296196+knkevol@users.noreply.github.com>
2026-01-14 15:15:25 +0900

    Reload 적용 완료(Network)

---
+++ Source/Robo_CopyProject/AnimNotify/AnimNotify_Reload.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotify_Reload.h"
+#include "../GameFramework/RoboPlayer.h"
+
+FString UAnimNotify_Reload::GetNotifyName_Implementation() const
+{
+	return TEXT("Reload");
+}
+
+void UAnimNotify_Reload::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	ARoboPlayer* RoboPlayer = Cast<ARoboPlayer>(MeshComp->GetOwner());
+	if (RoboPlayer)
+	{
+		RoboPlayer->ReloadWeapon();
+	}
+}
--- Source/Robo_CopyProject/Robo_CopyProject.Build.cs
+++ Source/Robo_CopyProject/Robo_CopyProject.Build.cs
@@ -8,7 +8,9 @@ public class Robo_CopyProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AnimGraphRuntime", "NavigationSystem", "AIModule", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", 
+			"EnhancedInput", "AnimGraphRuntime", "NavigationSystem", "AIModule", "UMG",
+			"AnimGraphRuntime"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
